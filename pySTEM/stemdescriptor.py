7616667a26119dc65b0fd5b019e2fe5a1c7eea53
Christoph Freysoldt <freysoldt@mpie.de>
2020-04-07 16:06:46 +0200

    use fft-based algorithm in stemdescriptor

--- pySTEM/stemdescriptor.py
+++ pySTEM/stemdescriptor.py
@@ -1,7 +1,8 @@
 import numpy as np
 import time
 import os
-import _stemdescriptor
+#import _stemdescriptor
+import stemdescriptor2 as fftstem
 
 
 
@@ -10,6 +11,26 @@ def get_num_shifts(window_x,window_y,grid):
     n_x = int(2*window_x/grid) + 1
     n_y = int(2*window_y/grid) + 1
     return n_x*n_y
+
+
+def good_fftsize(nn):
+
+   """" Decide whether this is a good size for FFTs: nn=2^n 3^m 5^k 7^l """
+   while (nn % 2 == 0):
+      nn /= 2
+   while (nn % 3 == 0):
+      nn /= 3
+   while (nn % 5 == 0):
+      nn /= 5
+   while (nn % 7 == 0):
+      nn /= 7
+   return (nn == 1)
+
+def get_good_fftsize(minsize):
+
+   while not good_fftsize(minsize):
+      minsize += 1
+   return minsize
 
 
 def get_descriptor(image, patch_x=11,patch_y=5,window_x=51,window_y=51,num_points=100,step=3,parallel=True,removing_mean=True):
@@ -35,6 +56,15 @@ def get_descriptor(image, patch_x=11,patch_y=5,window_x=51,window_y=51,num_point
     num_rows_desp = len(descriptors)
     num_cols_desp = len(descriptors[0])
     descriptors = descriptors.flatten()
-    _stemdescriptor.calc(np.reshape(image,-1),descriptors,num_rows,num_cols,patch_x,patch_y,
-                          window_x,window_y,grid,grid,n_descriptors,step,num_rows_desp,num_cols_desp,int(removing_mean))
+
+    # --- initialize fft
+    fftx = get_good_fftsize (2 * (window_x + patch_x) + 2)
+    ffty = get_good_fftsize (2 * (window_y + patch_y) + 2)
+    # note: fft has first dimension run fastest
+    fft = fftstem.WindowFFT (ffty, fftx)
+    # compute descriptor
+    # _stemdescriptor.calc(np.reshape(image,-1),descriptors,num_rows,num_cols,patch_x,patch_y,
+    #                      window_x,window_y,grid,grid,n_descriptors,step,num_rows_desp,num_cols_desp,int(removing_mean))
+    fft.calc(np.reshape(image,-1),descriptors,num_rows,num_cols,patch_x,patch_y,
+                          window_x,window_y,grid,grid,n_descriptors,step,num_rows_desp,num_cols_desp)
     return np.reshape(descriptors, (len(x_index), len(y_index), n_descriptors))
