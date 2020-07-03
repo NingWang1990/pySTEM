b59994d729b9fa01507075dcb5edd2d9ea6df73a
Ning Wang <wangning2nwpu@gmail.com>
2020-07-03 09:45:28 +0200

    add install_requires

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import numpy.distutils.misc_util
 from setuptools import find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
-__version__ = '0.0.20'
+__version__ = '0.0.21'
 
 setup(name='pystem',version=__version__,
       ext_modules=[Extension("_stemdescriptor", 
@@ -33,6 +33,10 @@ setup(name='pystem',version=__version__,
       maintainer = 'Ning Wang',
       maintainer_email = 'nwang@mpie.de',
       license = 'GNU GPLv3',
+      install_requires=[
+                        'numpy>=1.17.0',
+                        'scipy>=1.2.0',
+                        'scikit-learn>=0.21.0'],
       classifiers = [
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
